fda0ca8d89c2ee6c215befc52ae102f216817bd3
niida <54788301+naokiiida@users.noreply.github.com>
2024-10-04 16:34:51 +0900

    feat(helix): js, ts, tsx, jsx, json and apps script settings with biome

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -12,6 +12,27 @@ local keys = {
 	{ key = "^", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "_", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "+", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+  -- { key = 'j', mods = 'SUPER', action = wezterm.action.TogglePaneZoomState, },
+  {
+		key = 'j', mods = 'SUPER',
+    action = wezterm.action_callback(function(_, pane)
+        local tab = pane:tab()
+        local panes = tab:panes_with_info()
+        if #panes == 1 then
+            pane:split({
+                direction = "Right",
+                size = 0.4,
+            })
+        elseif not panes[1].is_zoomed then
+            panes[1].pane:activate()
+            tab:set_zoomed(true)
+        elseif panes[1].is_zoomed then
+            tab:set_zoomed(false)
+            panes[2].pane:activate()
+        end
+    end),
+	},
+
 }
 
 return keys
